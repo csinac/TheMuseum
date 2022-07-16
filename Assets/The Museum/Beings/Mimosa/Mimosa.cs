fcd9f2c46de90c839ce9c5842c2815872458c124
rectdev <goodrect@gmail.com>
2022-07-16 15:04:36 +0300

    Mimosa measures the average distance to nearby objects.

--- Assets/The Museum/Beings/Mimosa/Mimosa.cs
+++ Assets/The Museum/Beings/Mimosa/Mimosa.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +12,22 @@ namespace RectangleTrainer.MOIB.Installation
         }
 
         protected override void OnProximity(float[] values) {
-            Debug.Log(string.Join(", ", values));
+            int count = 0;
+            float sum = 0;
+            
+            for (int i = 0; i < values.Length; i++) {
+                if (values[i] <= 1) {
+                    sum += values[i];
+                    count++;
+                }
+            }
+            
+            if(count > 0)
+                sum /= count;
+            else
+                sum = Single.PositiveInfinity;
+
+            Debug.Log(sum);
         }
 
         protected override void OnAudio(float[] values) {
