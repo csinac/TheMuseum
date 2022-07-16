a55e326113496976645a04aa7895ddefd45df2b0
rectdev <goodrect@gmail.com>
2022-07-16 14:53:38 +0300

    Scene manager (curator) added to the scene, frame rate capped at 90

---
+++ Assets/The Museum/Scripts/Curator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RectangleTrainer.MOIB
+{
+    public class Curator : MonoBehaviour
+    {
+        void Start() {
+            Application.targetFrameRate = 90;
+        }
+    }
+}
