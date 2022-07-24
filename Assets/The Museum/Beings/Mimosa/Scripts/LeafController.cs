18881b66483fbeb62419eaee4b5c5f9329740abd
rectdev <cemsinacetin@gmail.com>
2022-07-24 16:53:48 +0300

    Mimosa leaf and stem animation moved to animation layers, scripted transform control removed.

--- Assets/The Museum/Beings/Mimosa/Scripts/LeafController.cs
+++ Assets/The Museum/Beings/Mimosa/Scripts/LeafController.cs
@@ -1,68 +1,37 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace RectangleTrainer.MOIB.Installation.Mimosa
 {
     public class LeafController : MonoBehaviour
     {
-        [SerializeField] private Transform root;
-        [SerializeField] private List<Transform> stem;
-        [SerializeField] private List<Transform> leavesL;
-        [SerializeField] private List<Transform> leavesR;
-        [Space]
-        [SerializeField] private float undulationRange = 10;
+        [SerializeField] private Animator animator;
+        [SerializeField] private AnimationCurve trustMapping = AnimationCurve.Linear(0, 0, 1, 1);
+        private float trust = 0.5f;
 
-        private float[] leafOffsetL;
-        private float[] leafOffsetR;
-
-        private Vector3[] leafInitL;
-        private Vector3[] leafInitR;
-
-        [ContextMenu("Populate Transforms")]
-        private void PopulateTransforms() {
-            stem = new List<Transform>();
-            leavesL = new List<Transform>();
-            leavesR = new List<Transform>();
-
-            Transform[] transforms = GetComponentsInChildren<Transform>();
-            foreach (var t in transforms) {
-                if(t.name.Contains("LeafL"))
-                    leavesL.Add(t);
-                else if(t.name.Contains("LeafR"))
-                    leavesR.Add(t);
-                else if(t.name.Contains("Stem"))
-                    stem.Add(t);
-            }
-        }
-
+        private static readonly int trustParamID = Animator.StringToHash("trust");
+        private int jiggleLayer;
+        
         private void Start() {
-            InitializeWaveTimes();
+            jiggleLayer = animator.GetLayerIndex("jigglejiggle");
+            SetTrust(0.5f);
         }
 
-        private void InitializeWaveTimes() {
-            leafOffsetL = new float[leavesL.Count];
-            leafOffsetR = new float[leavesL.Count];
+        public void SetTrust(float trust) {
+            this.trust = Mathf.Clamp(trust, 0f, 1f);
+            
+            animator.SetLayerWeight(jiggleLayer, Mathf.Clamp(this.trust - .25f, 0f, 1f) / 0.75f);
+            animator.SetFloat(trustParamID, 1 - this.trust);
+        }
 
-            leafInitL = new Vector3[leavesL.Count];
-            leafInitR = new Vector3[leavesL.Count];
-
-            for (int i = 0; i < leavesL.Count; i++) {
-                leafOffsetL[i] = Random.Range(-Mathf.PI, Mathf.PI);
-                leafOffsetR[i] = Random.Range(-Mathf.PI, Mathf.PI);
-
-                leafInitL[i] = leavesL[i].localEulerAngles;
-                leafInitR[i] = leavesR[i].localEulerAngles;
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                SetTrust(trust + 0.05f);
             }
-        }
-
-        private void LateUpdate() {
-            RotateLeaves();
-        }
-
-        private void RotateLeaves() {
-            for (int i = 0; i < leavesL.Count; i++) {
-                leavesL[i].localEulerAngles = leafInitL[i] + new Vector3(0, Mathf.Cos(leafOffsetL[i] + Time.time) * undulationRange, 0);
-                leavesR[i].localEulerAngles = leafInitR[i] + new Vector3(0, Mathf.Cos(leafOffsetR[i] + Time.time) * undulationRange, 0);
+            else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                SetTrust(trust - 0.05f);
             }
         }
     }
